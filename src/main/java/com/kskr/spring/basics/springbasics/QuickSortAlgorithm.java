1752fd0a9fa8197b0d80042462f90a368468b665
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-10-18 11:59:45 +0530

    Add quicksort algorithm

---
+++ src/main/java/com/kskr/spring/basics/springbasics/QuickSortAlgorithm.java
@@ -0,0 +1,8 @@
+package com.kskr.spring.basics.springbasics;
+
+public class QuickSortAlgorithm {
+    public int[] sort(int[] numbers) {
+        //logic for quick sort
+        return numbers;
+    }
+}
