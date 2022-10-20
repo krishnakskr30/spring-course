cebfeef73a84528d2454cbc2ee4307e373f87434
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-10-21 02:33:26 +0530

    Spring unit testing with xml configuration

---
+++ src/test/java/com/kskr/spring/basics/BinarySearchXmlTest.java
@@ -0,0 +1,24 @@
+package com.kskr.spring.basics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
+public class BinarySearchXmlTest {
+    @Autowired
+    BinarySearchImpl binarySearch;
+
+    @Test
+    public void exampleTest() {
+        int actualResult = binarySearch.binarySearch(new int[]{1, 2, 3}, 3);
+        int expectedResult = 3;
+
+        assertEquals(actualResult, expectedResult);
+    }
+}
