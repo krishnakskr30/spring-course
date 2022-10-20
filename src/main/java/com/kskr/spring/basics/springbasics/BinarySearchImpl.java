3f6793ca99ddf448340ec9e2ca3e09ab3393b482
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-10-20 11:24:46 +0530

    Refactor BinarySearchImpl to make it prototype

--- src/main/java/com/kskr/spring/basics/springbasics/BinarySearchImpl.java
+++ src/main/java/com/kskr/spring/basics/springbasics/BinarySearchImpl.java
@@ -2,9 +2,11 @@ package com.kskr.spring.basics.springbasics;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class BinarySearchImpl {
 
     @Autowired
