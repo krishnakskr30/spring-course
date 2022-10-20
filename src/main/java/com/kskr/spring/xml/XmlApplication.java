55ef75e73652da8fd0315cc2897ed0102d3baa13
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-10-20 21:43:24 +0530

    Refactor applicationContext.xml to load beans from annotated packages

--- src/main/java/com/kskr/spring/xml/XmlApplication.java
+++ src/main/java/com/kskr/spring/xml/XmlApplication.java
@@ -1,5 +1,7 @@
 package com.kskr.spring.xml;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -7,12 +9,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 @Configuration
 @ComponentScan("com.kskr.spring.basics")
 public class XmlApplication {
+    static Logger LOGGER = LoggerFactory.getLogger(XmlApplication.class);
+
     public static void main(String[] args) {
         try (
                 ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml")
         ) {
+            LOGGER.info("Beans Loaded->{}", (Object) applicationContext.getBeanDefinitionNames());
             XmlDAO xmlDAO = applicationContext.getBean(XmlDAO.class);
-            System.out.println(xmlDAO);
+            LOGGER.info("{} {}", xmlDAO, xmlDAO.getXmlJdbcConnection());
         }
     }
 }
