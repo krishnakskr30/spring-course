3dd09beee16f36f04caff7e9aa129dd5eed946bc
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-10-20 14:59:40 +0530

    Add component scan to learn ComponentScan annotation

---
+++ src/main/java/com/kskr/spring/componentscan/ComponentDAO.java
@@ -0,0 +1,19 @@
+package com.kskr.spring.componentscan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class ComponentDAO {
+    @Autowired
+    ComponentJdbcConnection componentJdbcConnection;
+
+    public ComponentJdbcConnection getComponentJdbcConnection() {
+        return componentJdbcConnection;
+    }
+
+    public void setComponentJdbcConnection(ComponentJdbcConnection componentJdbcConnection) {
+        this.componentJdbcConnection = componentJdbcConnection;
+    }
+}
---
+++ src/main/java/com/kskr/spring/componentscan/ComponentScanApplication.java
@@ -0,0 +1,22 @@
+package com.kskr.spring.componentscan;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.kskr.spring.componentscan") //Search for components
+public class ComponentScanApplication {
+    static Logger LOGGER = LoggerFactory.getLogger(ComponentScanApplication.class);
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(ComponentScanApplication.class, args);
+
+        ComponentDAO componentDAO = applicationContext.getBean(ComponentDAO.class);
+
+        LOGGER.info("{}", componentDAO);
+    }
+}
