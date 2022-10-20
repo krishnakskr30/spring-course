ff177f96c3549876f4568b4f3365fec6994ffc1a
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-10-20 11:55:57 +0530

    Refactor JdbcConnection to use it as prototype with singleton PersonDAO

---
+++ src/main/java/com/kskr/spring/scope/SpringScopeApplication.java
@@ -0,0 +1,25 @@
+package com.kskr.spring.scope;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringScopeApplication {
+    static Logger LOGGER = LoggerFactory.getLogger(SpringScopeApplication.class);
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(SpringScopeApplication.class, args);
+
+        PersonDAO personDAO = applicationContext.getBean(PersonDAO.class);
+        PersonDAO personDAO2 = applicationContext.getBean(PersonDAO.class);
+
+        LOGGER.info("{}", personDAO);
+        LOGGER.info("{}", personDAO.getJdbcConnection());
+
+        LOGGER.info("{}", personDAO2);
+        LOGGER.info("{}", personDAO2.getJdbcConnection());
+    }
+}
